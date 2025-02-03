ea0d4bd3981ea6a01a3494d7bf14f4cfcd4dae55
linck loic <loic.linck6@etu.univ-lorraine.com>
2025-02-03 11:04:54 +0100

    ajout analyze_logs

---
+++ analyze_logs.py
@@ -0,0 +1,56 @@
+from pyspark import SparkContext
+import re
+
+# Initialiser le contexte Spark
+sc = SparkContext(appName="AnalyseLogsApacheRDD")
+
+# Charger les logs depuis HDFS
+log_file = "hdfs://sparkhdfs-namenode-1:9000/logs/web_server.log"
+logs_rdd = sc.textFile(log_file)
+
+# APicher les 10 premières lignes
+print("Exemple de lignes du fichier de logs :")
+for line in logs_rdd.take(10):
+    print(line)
+
+
+# Définir le pattern regex pour parser les logs
+log_pattern = r'(\d+\.\d+\.\d+\.\d+) - - \[(.*?)\] "(.*?) (.*?) HTTP.*" (\d+) (\d+)'
+# Fonction pour parser une ligne de log
+def parse_log_line(line):
+    match = re.match(log_pattern, line)
+    if match:
+        return (
+            match.group(1), # IP
+            match.group(2), # Timestamp
+            match.group(3), # HTTP Method
+            match.group(4), # URL
+            int(match.group(5)), # HTTP Status
+            int(match.group(6)) # Response Size
+        )
+    else:
+        return None
+
+# Parser les logs
+parsed_logs_rdd = logs_rdd.map(parse_log_line).filter(lambda x: x is not
+None)
+
+# Afficher 10 logs parsés
+print("Exemple de logs parsés :")
+for log in parsed_logs_rdd.take(10):
+    print(log)
+
+total_requests = parsed_logs_rdd.count()
+print(f"Nombre total de requêtes : {total_requests}")
+
+top_urls = parsed_logs_rdd.map(lambda x: (x[3], 1)).reduceByKey(lambda a, b: a + b).takeOrdered(5,
+key=lambda x: -x[1])
+print("Les 5 URLs les plus demandées :")
+for url, count in top_urls:
+ print(f"{url}: {count}")
+
+'''
+nombre de requete pour http
+la taille moyenne des reponses par code http
+adresse ip la plus active 
+'''
